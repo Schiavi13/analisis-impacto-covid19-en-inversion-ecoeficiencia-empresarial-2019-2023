4e260274fd6d9c62ba5232c7daddae637ddb499e
LUIS FERNANDO <lufmartinezmu@gmail.com>
2026-04-05 22:25:57 -0500

    Modifica conector a mysql

--- src/connection.py
+++ src/connection.py
@@ -1,10 +1,5 @@
 import streamlit as st
-from mysql.connector import Error
 
 def get_connection():
-    try:
-        connection = st.connection('mysql', type='sql')  
-    except Error as e:
-        print(f"ERROR DE CONEXIÓN: {e}") 
-        return None
+    connection = st.connection('mysql', type='sql')  
     return connection
