a41baf6cfb41dc699810f33a74e6b4b7cd00516d
LUIS FERNANDO <lufmartinezmu@gmail.com>
2026-04-06 17:17:38 -0500

    Corrige llamado a calculo gasto_ambiental

--- pages/6_database.py
+++ pages/6_database.py
@@ -3,7 +3,7 @@ from src.components import calcular_gasto_amb_total
 
 # Perform query.
 
-df = calcular_gasto_amb_total
+df = calcular_gasto_amb_total()
 
 # Print results.
 st.write(f"{df['gasto_gestion_amb_total']}")
