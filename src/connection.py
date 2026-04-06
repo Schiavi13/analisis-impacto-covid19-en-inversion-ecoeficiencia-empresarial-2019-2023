6ddcb86afd03aa3b46b0d9096162595fc57c50a7
LUIS FERNANDO <lufmartinezmu@gmail.com>
2026-04-05 21:56:28 -0500

    Ágrega conexión a base de datos remota

--- src/connection.py
+++ src/connection.py
@@ -1,23 +1,11 @@
-import mysql.connector
+import streamlit as st
 from mysql.connector import Error
-import os
-from dotenv import load_dotenv
-
-# Cargamos las variables del archivo secreto .env
-load_dotenv()
 
 def get_connection():
     try:
-        connection = mysql.connector.connect(
-            host=os.getenv("DB_HOST"),
-            port=int(os.getenv("DB_PORT")),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-            database=os.getenv("DB_NAME"),
-            auth_plugin='mysql_native_password'
-        )
+        connection = st.connection('mysql', type='sql')
         if connection.is_connected():
             return connection
     except Error as e:
-        print(f"❌ ERROR TÉCNICO DE CONEXIÓN: {e}") 
+        print(f"ERROR DE CONEXIÓN: {e}") 
         return None
