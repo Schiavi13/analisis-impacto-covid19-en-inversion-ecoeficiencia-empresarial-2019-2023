2b727c2fd8526f3d37d92f81140569c7a42dc50c
LUIS FERNANDO <lufmartinezmu@gmail.com>
2026-04-05 22:31:37 -0500

    Extrae sector economico

--- pages/6_database.py
+++ pages/6_database.py
@@ -7,4 +7,4 @@ df = conn.query('SELECT * from sector_economico;', ttl=600)
 
 # Print results.
 for row in df.itertuples():
-    st.write(f"{row.id_sector}, {row.sector_nombre}:")
+    st.write(f"{row.id_sector}, {row.nombre_sector}:")
