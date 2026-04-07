dd72509aad3e6828c7a1cc0f6f7b926e53bea6a4
LUIS FERNANDO <lufmartinezmu@gmail.com>
2026-04-07 16:40:28 -0500

    fuerza cierre conexión

--- src/connection.py
+++ src/connection.py
@@ -3,4 +3,10 @@ import streamlit as st
 
 def get_connection():
     connection = st.connection('mysql', type='sql')  
-    return connection
+    connection.session.close()   # Close the SQLAlchemy session
+    connection.engine.dispose()
+    return None
+
+def close_connection(conn):
+    conn.session.close()   # Close the SQLAlchemy session
+    conn.engine.dispose()
